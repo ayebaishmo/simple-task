58ef6e1d6f7b1faf5198f73d5b3f551bf974068b
ayebaishmo <ishmo255super@gmail.com>
2024-04-07 21:45:09 +0300

    Created a function that prints out a task

--- simple/read-tasks.py
+++ simple/read-tasks.py
@@ -1,5 +1,19 @@
 class Tasks:
-    def created_tasks (self, display):
-        self.display = display
-        
+    def __init__(self, tasks_display):
+        self.tasks_display = tasks_display 
+    
+    # def create_task(self, create):
+    #     self.display.append("First Task")
+    #     return self.display
+
+    def view_tasks(self):
+        self.view = self.display
+        return self.view
+    
+    def __repr__(self):
+        return self.tasks_display
+    
+if __name__=='__main__':
+    Task1 = Tasks("First Task")
+    print(Task1)
         
