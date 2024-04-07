68af8752a8da83a1e9611ab425f0d070c1bc378e
ayebaishmo <ishmo255super@gmail.com>
2024-04-07 22:46:25 +0300

    Added a meythod that deletes a task

--- simple/read-tasks.py
+++ simple/read-tasks.py
@@ -1,19 +1,28 @@
 class Tasks:
     def __init__(self, tasks_display):
-        self.tasks_display = tasks_display 
+        self.tasks_display = tasks_display
+        self.all_tasks = []
     
-    # def create_task(self, create):
-    #     self.display.append("First Task")
-    #     return self.display
-
-    def view_tasks(self):
-        self.view = self.display
-        return self.view
+    def create_task(self, create):
+        self.all_tasks.append(create)
+        return self.all_tasks
+    
+    def remove_task(self, dele):
+        self.all_tasks.remove(dele)
+        return self.all_tasks
+    
+    def list_display_tasks(self):
+        for task in self.all_tasks:
+            return task
     
     def __repr__(self):
         return self.tasks_display
     
 if __name__=='__main__':
-    Task1 = Tasks("First Task")
-    print(Task1)
+    Task1 = Tasks("Hello")
+    print(Task1.create_task("First task"))
+    print(Task1.create_task("Second tasks"))
+    print(Task1.create_task("Third tasks"))
+    print(Task1.remove_task("First task"))
+    print(Task1.list_display_tasks())
         
